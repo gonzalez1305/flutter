8bdd359a60365275cf001149d3191b1fe1e1cd33
gonzalez1305 <carolg998488@gmail.com>
2024-04-01 10:49:11 -0500

    se crea la clase repaso y prueba

---
+++ lib/paginas/Prueba.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PruebaPage extends StatelessWidget {
+  const PruebaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: const Text('Pagina Prueba'),
+      ),
+    );
+  }
+}
